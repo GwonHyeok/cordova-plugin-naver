d3d9690567be17ab1a3c88748168a85f7310265b
GwonHyeok <me@ghyeok.io>
2017-07-27 12:02:03 +0900

    네이버 ios 플러그인 인앱 로그인, 계정 연동해제 기능이 작동하지 않던 버그 수정

--- src/ios/NaverPlugin.h
+++ src/ios/NaverPlugin.h
@@ -1,11 +1,23 @@
 #import <Foundation/Foundation.h>
 #import <Cordova/CDV.h>
-#import "AppDelegate.h"
 #import "NaverThirdPartyLoginConnection.h"
+#import "NLoginThirdPartyOAuth20InAppBrowserViewController.h"
 
-@interface NaverPlugin : CDVPlugin {
-  // Member variables go here.
+@interface NaverPlugin : CDVPlugin <NaverThirdPartyLoginConnectionDelegate> {
 }
 
-- (void)coolMethod:(CDVInvokedUrlCommand*)command;
+- (void)login:(CDVInvokedUrlCommand *)command;
+
+- (void)logout:(CDVInvokedUrlCommand *)command;
+
+- (void)logoutAndDeleteToken:(CDVInvokedUrlCommand *)command;
+
+- (void)refreshAccessToken:(CDVInvokedUrlCommand *)command;
+
+- (void)getState:(CDVInvokedUrlCommand *)command;
+
+- (void)requestApi:(CDVInvokedUrlCommand *)command;
+
+- (void)requestMe:(CDVInvokedUrlCommand *)command;
+
 @end
--- src/ios/NaverPlugin.m
+++ src/ios/NaverPlugin.m
@@ -14,7 +14,7 @@
     [NaverThirdPartyLoginConnection getSharedInstance].delegate = self;
 
     // 네이버 앱과, 인앱 브라우저 인증을 둘다 사용하도록 설정
-    // [[NaverThirdPartyLoginConnection getSharedInstance] setIsNaverAppOauthEnable:YES];
+    [[NaverThirdPartyLoginConnection getSharedInstance] setIsNaverAppOauthEnable:YES];
     [[NaverThirdPartyLoginConnection getSharedInstance] setIsInAppOauthEnable:YES];
 
     // 세로 화면 고정 설정
@@ -62,14 +62,12 @@
  * @param command
  */
 - (void)logoutAndDeleteToken:(CDVInvokedUrlCommand *)command {
-    [self.commandDelegate runInBackground:^{
-        // 콜백 아이디 설정
-        self.loginCallbackId = command.callbackId;
+    // 콜백 아이디 설정
+    self.loginCallbackId = command.callbackId;
 
-        // 로그아웃 요청
-        NaverThirdPartyLoginConnection *loginConnection = [NaverThirdPartyLoginConnection getSharedInstance];
-        [loginConnection requestDeleteToken];
-    }];
+    // 로그아웃 요청
+    NaverThirdPartyLoginConnection *loginConnection = [NaverThirdPartyLoginConnection getSharedInstance];
+    [loginConnection requestDeleteToken];
 }
 
 /**
@@ -78,14 +76,12 @@
  * @param command
  */
 - (void)refreshAccessToken:(CDVInvokedUrlCommand *)command {
-    [self.commandDelegate runInBackground:^{
-        // 콜백 아이디 설정
-        self.loginCallbackId = command.callbackId;
+    // 콜백 아이디 설정
+    self.loginCallbackId = command.callbackId;
 
-        // AccessToken 재 발급 요청
-        NaverThirdPartyLoginConnection *loginConnection = [NaverThirdPartyLoginConnection getSharedInstance];
-        [loginConnection requestAccessTokenWithRefreshToken];
-    }];
+    // AccessToken 재 발급 요청
+    NaverThirdPartyLoginConnection *loginConnection = [NaverThirdPartyLoginConnection getSharedInstance];
+    [loginConnection requestAccessTokenWithRefreshToken];
 }
 
 /**
@@ -99,12 +95,25 @@
     [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
 }
 
+/**
+ * GET 메서드로 API를 호출합니다. 성공하면 결과(content body)를 반환합니다.
+ *
+ * @param command
+ */
 - (void)requestApi:(CDVInvokedUrlCommand *)command {
     // TODO 구현 필요, 안드로이드 라이브러리에는 있으나 아이폰 라이브러리에는 존재하지 않는 메소드
     CDVPluginResult *pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"구현되지 않았습니다"];
     [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
 }
 
+/**
+ * 네이버 로그인을 통해 인증받은 받고 정보 제공에 동의한 회원에 대해 회원 메일 주소, 별명, 프로필 사진, 생일, 연령대 값을 조회할 수 있는 로그인 오픈 API입니다.
+ * API 호출 결과로 네이버 아이디값은 제공하지 않으며, 대신 'id'라는 애플리케이션당 유니크한 일련번호값을 이용해서 자체적으로 회원정보를 구성하셔야 합니다.
+ * 기존 REST API처럼 요청 URL과 요청 변수로 호출하는 방법은 동일하나, OAuth 2.0 인증 기반이므로 추가적으로 네이버 로그인 API를 통해 접근 토큰(access token)을 발급받아,
+ * HTTP로 호출할 때 Header에 접근 토큰 값을 전송해 주시면 활용 가능합니다.
+ *
+ * @param command
+ */
 - (void)requestMe:(CDVInvokedUrlCommand *)command {
     NSString *accessToken = [[NaverThirdPartyLoginConnection getSharedInstance] accessToken];
 
@@ -117,23 +126,26 @@
 
     [[[NSURLSession sharedSession] dataTaskWithRequest:urlRequest completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
         NSError *serializationError;
-        NSDictionary *json = [NSJSONSerialization JSONObjectWithData:data
-                                                             options:nil
-                                                               error:&serializationError];
+        NSDictionary *json = [NSJSONSerialization JSONObjectWithData:data options:nil error:&serializationError];
 
         CDVPluginResult *pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsDictionary:json];
         [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
     }] resume];
 }
 
+#pragma mark - Utility methods
+
+- (void)presentWebviewControllerWithRequest:(NSURLRequest *)urlRequest {
+    NLoginThirdPartyOAuth20InAppBrowserViewController *inAppBrowserViewController = [[NLoginThirdPartyOAuth20InAppBrowserViewController alloc] initWithRequest:urlRequest];
+    inAppBrowserViewController.parentOrientation = (UIInterfaceOrientation) [[UIDevice currentDevice] orientation];
+    [[self viewController] presentViewController:inAppBrowserViewController animated:NO completion:nil];
+}
+
+#pragma mark - NaverThirdPartyLoginConnectionDelegate
 
 - (void)oauth20ConnectionDidOpenInAppBrowserForOAuth:(NSURLRequest *)request {
     NSLog(@"oauth20ConnectionDidOpenInAppBrowserForOAuth");
-    NSString *accessToken = [[NaverThirdPartyLoginConnection getSharedInstance] accessToken];
-    CDVPluginResult *pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:accessToken];
-    [self.commandDelegate sendPluginResult:pluginResult callbackId:self.loginCallbackId];
-
-    self.loginCallbackId = nil;
+    [self presentWebviewControllerWithRequest:request];
 }
 
 - (void)oauth20ConnectionDidFinishRequestACTokenWithAuthCode {
